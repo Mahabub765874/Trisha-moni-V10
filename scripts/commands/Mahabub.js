bc5ef61fc3cd309be0c0f9371156e49b2dc11585
Mahabub443 <164204019+Mahabub443@users.noreply.github.com>
2024-03-27 09:44:51 +0600

    Update Mahabub.js

--- scripts/commands/Mahabub.js
+++ scripts/commands/Mahabub.js
@@ -4,7 +4,7 @@ module.exports.config = {
   permission: 0,
   credits: "Imran Ahmed_X_Mahabub Rahman",
   description: "example",
-  prefix: false,
+  prefix: true,
   category: "Fun", 
   usages: "user", 
   cooldowns: 5,
@@ -22,7 +22,7 @@ const fs = global.nodemodule["fs-extra"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
 
-  "https://drive.google.com/uc?id=1B31nfigyaYEyKhkDeRZNn5Nab8As6NjP","https://drive.google.com/uc?id=1B6ZnpXOkwV1jqjhjGECv3i8Z4lsi0rLb"
+  ""https://i.imgur.com/Mwh9u6a.mp4",","https://drive.google.com/uc?id=1B31nfigyaYEyKhkDeRZNn5Nab8As6NjP","https://drive.google.com/uc?id=1B6ZnpXOkwV1jqjhjGECv3i8Z4lsi0rLb"
 ];
      var callback = () => api.sendMessage({body:`「 ${know} 」`,attachment: fs.createReadStream(__dirname + "/cache/15.mp4")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/15.mp4"));    
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/15.mp4")).on("close",() => callback());
